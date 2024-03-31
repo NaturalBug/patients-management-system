b37521132ac7d0b5b0783835a4746be66c0d5c8b
Cheng-Ying Li <chengying.li@bridgewell.com>
2024-03-31 21:51:28 +0800

    feat: initialize ApolloClient

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const client = new ApolloClient({
+  uri: 'http://localhost:8080',
+  cache: new InMemoryCache()
+});
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
